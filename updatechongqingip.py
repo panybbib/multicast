aed2991c0e6df934cd2a525cf8bd9ee6f92b2f8c
panybbib <pan.107@163.com>
2026-02-25 18:17:43 +0800

    Update updatechongqingip.py

--- updatechongqingip.py
+++ updatechongqingip.py
@@ -123,9 +123,9 @@ def compute_scores(results):
             norm_latency = 0
 
         score = (
-            0.6 * norm_tp +
-            0.3 * (1 - norm_latency) +
-            0.1 * (1 - r["loss"])
+            0.65 * norm_tp +
+            0.25 * (1 - norm_latency) +
+            0.10 * (1 - r["loss"])
         )
 
         scores[ip] = score
@@ -221,4 +221,5 @@ update_files(primary, files_group_1)
 update_files(secondary, files_group_2)
 
 print("\n完成")
+
 
